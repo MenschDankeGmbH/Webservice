b126c719a2a32952767ceb5fce2624236a794c8d
Stefan D <stefan@php-engineer.de>
2016-02-11 22:12:25 +0100

    schema bugfix

--- src/Model/Endpoint.php
+++ src/Model/Endpoint.php
@@ -772,24 +772,16 @@ class Endpoint implements RepositoryInterface, EventListenerInterface, EventDisp
             return false;
         }
 
+        $data = $resource->extract($this->schema()->columns(), true);
+
         if ($resource->isNew()) {
-            $query = $this->query()->create()->set($resource->toArray());
+            $query = $this->query()->create();
         } else {
             $query = $this->query()->update()->where([
                 $this->primaryKey() => $resource->get($this->primaryKey())
             ]);
-
-            $fieldsToUpdate = [];
-            foreach ($resource->toArray() as $field => $value) {
-                if (!$resource->dirty($field)) {
-                    continue;
-                }
-
-                $fieldsToUpdate[$field] = $value;
-            }
-
-            $query->set($fieldsToUpdate);
         }
+        $query->set($data);
 
         $result = $query->execute();
         if (!$result) {
